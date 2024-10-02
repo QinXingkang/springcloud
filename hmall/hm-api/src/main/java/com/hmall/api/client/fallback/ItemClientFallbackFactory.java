ba59e7b1202bf21f2717087cae16a3fcc0d455be
QinXingkang <2505083096@qq.com>
2024-10-02 12:48:10 +0800

    配置管理-结束

---
+++ hmall/hm-api/src/main/java/com/hmall/api/client/fallback/ItemClientFallbackFactory.java
@@ -0,0 +1,36 @@
+package com.hmall.api.client.fallback;
+
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
+import com.hmall.common.utils.CollUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ItemClientFallbackFactory implements FallbackFactory<ItemClient> {
+
+    private static final Logger log = LoggerFactory.getLogger(ItemClientFallbackFactory.class);
+
+    @Override
+    public ItemClient create(Throwable cause) {
+        return new ItemClient() {
+            @Override
+            public List<ItemDTO> queryItemByIds(Collection<Long> ids) {
+                log.error("查询商品失败",cause);
+                return CollUtils.emptyList();
+            }
+
+            @Override
+            public void deductStock(List<OrderDetailDTO> items) {
+                log.error("扣减商品库存失败",cause);
+                // TODO 减库存业务
+                throw new RuntimeException(cause);
+
+            }
+        };
+    }
+}
