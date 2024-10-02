71864505a8097efc73261c123d20f76bed23d1be
QinXingkang <2505083096@qq.com>
2024-10-02 23:44:53 +0800

    分布式事务

---
+++ mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -0,0 +1,39 @@
+package com.itheima.consumer.mq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+@Slf4j
+public class SpringRabbitListener {
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenSimpleQueue(String message){
+        log.info("监听到simple.queue的消息：[{}]",message);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String message) throws InterruptedException {
+        System.out.println("消费者1接收到消息：" + message + "，" + LocalTime.now());
+        Thread.sleep(25);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue(String message) throws InterruptedException {
+        System.err.println("消费者2接收到消息：" + message + "，" + LocalTime.now());
+        Thread.sleep(200);
+    }
+
+    @RabbitListener(queues = "fanout.queue1")
+    public void listenFanoutQueue1(String message){
+        log.info("消费者1监听到fanout.queue1的消息：[{}]",message);
+    }
+
+    @RabbitListener(queues = "fanout.queue2")
+    public void listenFanoutQueue2(String message){
+        log.info("消费者2监听到fanout.queue2的消息：[{}]",message);
+    }
+}
---
+++ mq-demo/publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -0,0 +1,33 @@
+package com.itheima.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringAmqpTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testWorkQueue(){
+        String queueName = "work.queue";
+        String message;
+        for (int i = 0; i < 50; i++) {
+            message = "Hello SpringAMQP" + " " + (i + 1);
+            rabbitTemplate.convertAndSend(queueName,message);
+        }
+    }
+
+    @Test
+    public void testFanoutQueue(){
+        String exchangeName = "hmall.fanout";
+        String message = "hello everyone";
+
+        rabbitTemplate.convertAndSend(exchangeName,null,message);
+    }
+}
